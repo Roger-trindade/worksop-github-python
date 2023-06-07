20c9ba38e6b9844198c140757df55121d3d4eccc
roger-trindade <rogerv.t18@gmailm>
2023-06-06 21:49:34 -0300

    cria uma nova variavel para idade

--- nem.py
+++ nem.py
@@ -1,7 +1,7 @@
 name = input('dite seu nome. ')
 city = input('digite a cidade. ')
 gender = input('digite o sexo. ')
-
+age = input('digite sua idade') 
 print('segue os dados: ')
-print(name, ' - ', city, ' - ', gender)
+print(name, ' - ', city, ' - ', gender, ' - ', age)
 
